8d102cd99a88047af4804ae2ee6c4bce1a1ed5a0
Hannah Willoughby <hannah@tcah.com>
2023-05-03 09:37:40 -0400

    Add theme provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-import { Container } from '@mui/material'
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
-import { Image } from 'mui-image'
+import { ParallaxProvider } from 'react-scroll-parallax'
+import LandingPage from './pages/LandingPage';
+import NavBar from './components/NavBar';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    }
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <main>
+      <ThemeProvider theme={theme}>
         <ParallaxProvider>
-          <Parallax style={{background: 'black'}} speed={-10}>
-            <Image src="https://i.ytimg.com/vi/23pYfDKyfBk/maxresdefault.jpg" />
-          </Parallax>
-          <Parallax style={{background: 'black'}} speed={5}>
-            <Container style={{display: 'flex', justifyContent: 'space-between'}}>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.s</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.</p>
-            </Container>
-          </Parallax>
+          <NavBar />
+          <LandingPage />
         </ParallaxProvider>
-      </main>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import AppBar from '@mui/material/AppBar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Toolbar from '@mui/material/Toolbar'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Box from '@mui/material/Box'
+import MenuIcon from '@mui/icons-material/Menu'
+import IconButton from '@mui/material/IconButton'
+import AdbIcon from '@mui/icons-material/Adb'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+
+function NavBar() {
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  }
+
+  const pages = []
+
+  return (
+    <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component='a'
+              href='/'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'inherit',
+                textDecoration: 'none'
+              }}
+            >
+              Nosey Neighbor
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                      <Link href={page.url} underline='none' color='inherit'>
+                        {page.name}
+                      </Link>
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Nosy Neighbor
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.name}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, mx: 1, color: 'white', display: 'block' }}
+                  href={page.url}
+                >
+                  {page.name}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+  )
+}
+
+export default NavBar
