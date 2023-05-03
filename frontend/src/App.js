aa0198ae641cf05c1288c79943775b42caf63f91
Hannah Willoughby <hannah@tcah.com>
2023-05-03 09:20:00 -0400

    Play around with parallax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from '@mui/material'
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
+import { Image } from 'mui-image'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <ParallaxProvider>
+          <Parallax style={{background: 'black'}} speed={-10}>
+            <Image src="https://i.ytimg.com/vi/23pYfDKyfBk/maxresdefault.jpg" />
+          </Parallax>
+          <Parallax style={{background: 'black'}} speed={5}>
+            <Container style={{display: 'flex', justifyContent: 'space-between'}}>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.s</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam. Facilisis volutpat est velit egestas dui id. Vestibulum morbi blandit cursus risus at ultrices. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Cursus mattis molestie a iaculis at. Vel pretium lectus quam id leo in. Dignissim convallis aenean et tortor at risus viverra adipiscing. Erat nam at lectus urna duis convallis. Rutrum tellus pellentesque eu tincidunt. Augue neque gravida in fermentum et sollicitudin. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Nulla aliquet enim tortor at auctor urna. Malesuada bibendum arcu vitae elementum.</p>
+            </Container>
+          </Parallax>
+        </ParallaxProvider>
+      </main>
     </div>
   );
 }
