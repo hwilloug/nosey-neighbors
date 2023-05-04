7fae3323a1c1e0bd080377f46b2fbf6cc3854316
Hannah Willoughby <hannah@tcah.com>
2023-05-04 18:29:37 -0400

    Add name in header image, styling

--- frontend/src/components/Music.js
+++ frontend/src/components/Music.js
@@ -4,8 +4,8 @@ import YoutubeEmbed from '../components/YoutubeEmbed'
 
 function Music() {
     return (
-        <Container sx={{mt: '100px'}} id="music">
-            <Typography variant='h4'>Music</Typography>
+        <Container id="music">
+            <Typography variant='h4' sx={{mt: '50px'}}>Music</Typography>
             <Container sx={{display: 'flex', gap: '20px', alignItems: 'center'}}>
                 <YoutubeEmbed embedId='q2I0ulTZWXA' />
                 <Spotify link="https://open.spotify.com/album/4uNzDULQ9RAy0nxGaPbCDn?si=Uh6oIUpuRaeuEybngX37rw" />
